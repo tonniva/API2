04abb4467c3e25461fca261891c4c9998249e707
Tonniva <tongiggabite@gmail.com>
2018-11-10 23:41:07 +0700

    update code Rest node with azure

---
+++ server.js
@@ -0,0 +1,39 @@
+// db.js 
+var mssql = require("mssql");
+var dbConfig = {
+    user: 'stg-product',
+    password: 'Tonniva016449054',
+    server: 'stg-product.database.windows.net',
+    database: 'stg-product',
+    options: {
+        encrypt: true
+    }
+};
+
+var connection = mssql.connect(dbConfig, function(err) {
+    if (err)
+        throw err;
+});
+
+module.exports = connection;
+
+// app.js 
+var db = require("mssql");
+var express = require("express");
+var app = express();
+
+app.get('/books', function(req, res, next) {
+    var request = new db.Request();
+    request.query('USE [stg-product]  SELECT * FROM [dbo].[UserDetail]', function(err, result) {
+        if (err)
+            return next(err);
+
+        var data = {};
+        data["user"] = result.recordset;
+        res.send(data);
+    });
+});
+
+var server = app.listen(5000, function() {
+    console.log('Server is running..');
+});
