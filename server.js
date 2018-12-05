c67f71a7fb23ddf3ccff3be7f755d1c3c98bad5e
Tonniva <tongiggabite@gmail.com>
2018-12-05 23:38:47 +0700

    test 11

--- server.js
+++ server.js
@@ -19,6 +19,12 @@ app.use(cors(corsOptions));
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //     next();
 // });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 var dbConfig = {
     user: 'stg-product',
@@ -39,12 +45,6 @@ module.exports = connection;
 // app.js 
 
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
 
 
 
