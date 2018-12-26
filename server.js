c7d4552b5178f7693406ed67c99ad1fb742c27cc
Tonniva <tongiggabite@gmail.com>
2018-12-26 11:36:10 +0700

    test20181226

--- server.js
+++ server.js
@@ -28,15 +28,24 @@ var app = express();
 var cors = require('cors');
 // app.use(cors);
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    // res.setHeader("Content-Type", "application/json"); 
-    res.statusCode = 404;
-    // res.end();
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     // res.setHeader("Content-Type", "application/json"); 
+//     res.statusCode = 404;
+//     // res.end();
 
+//     next();
+// });
+
+app.use(function(req, res, next) {
+    //Enabling CORS 
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization, x-access-token");
     next();
 });
+
 
 
 app.get('/books', function(req, res, next) {
