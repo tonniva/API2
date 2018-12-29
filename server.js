c5f19e646e6c6ade185d5b912c32a5a7f70ab273
Tonniva <tongiggabite@gmail.com>
2018-12-29 10:57:00 +0700

    test

--- server.js
+++ server.js
@@ -38,11 +38,17 @@ var cors = require('cors');
 //     next();
 // });
 
+// app.use(function(req, res, next) {
+//     //Enabling CORS 
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization, x-access-token");
+//     next();
+// });
+
 app.use(function(req, res, next) {
-    //Enabling CORS 
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization, x-access-token");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
