80da1950e6785ccbdd47142662ceec474e9ff7e0
Tonniva <tongiggabite@gmail.com>
2018-12-29 13:01:59 +0700

    1337

--- server.js
+++ server.js
@@ -263,7 +263,8 @@ function uniqueImage() {
 
 
 // var http = require('http')
-var port = process.env.PORT || 1337;
+var port = process.env.PORT;
+// var port = process.env.PORT || 1337;
 // http.createServer(function(req, res) {
 //     res.writeHead(200, { 'Content-Type': 'text/plain' });
 //     res.end('Hello World\n');
