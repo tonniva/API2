327cbcddd4d18b12768e4420d01b9e3cbf6c6916
Tonniva <tongiggabite@gmail.com>
2018-12-30 15:48:36 +0700

    ssssss

--- server.js
+++ server.js
@@ -142,11 +142,11 @@ app.post('/books', urlencodedParser, function(req, res, next) {
 });
 
 // var http = require('http')
-var port = process.env.PORT || 1337;
-// http.createServer(function(req, res) {
-//     res.writeHead(200, { 'Content-Type': 'text/plain' });
-//     res.end('Hello World\n');
-// }).listen(port);
+var port = process.env.port || process.env.PORT || 1337
+    // http.createServer(function(req, res) {
+    //     res.writeHead(200, { 'Content-Type': 'text/plain' });
+    //     res.end('Hello World\n');
+    // }).listen(port);
 
 
 
